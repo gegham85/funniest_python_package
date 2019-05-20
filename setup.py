b16bcb3de6e1574994dde663f946231350cb8986
Gegham Movses <gegham_m@yahoo.com>
2019-05-19 18:24:23 -0700

    change setup name

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class Test_cmd(Command):
 
 
 setup(
-    name='funniest',
+    name='funniest_test',
     version='0.1',
     description='The funniest joke in the world for learning',
     long_description=readme(),
