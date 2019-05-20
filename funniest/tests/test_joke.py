b97daf7fd638f3c565f41c0fa11262d5e365b7cb
Gegham Movses <gegham_m@yahoo.com>
2019-05-19 18:15:31 -0700

    first commit

---
+++ funniest/tests/test_joke.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+import funniest
+
+class TestJoke(TestCase):
+    def test_is_string(self):
+        s = funniest.joke()
+        self.assertTrue(isinstance(s, str))
+
+    def test_is_true(self):
+        self.assertTrue(True)
